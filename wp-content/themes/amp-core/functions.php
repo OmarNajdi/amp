75aa81e1d27bef0c4a41ae695d5a4b7fe88fae02
Omar Najdi <omarnajdi@gmail.com>
2021-08-23 09:50:25 +0300

    General Fixes

--- wp-content/themes/amp-core/functions.php
+++ wp-content/themes/amp-core/functions.php
@@ -28,8 +28,9 @@ if ( ! function_exists( 'amp_load_styles_and_scripts' ) ) {
 		// Remove Block Editor Styles
 		wp_dequeue_style( 'wp-block-library' );
 		wp_dequeue_style( 'wp-block-library-theme' );
-		wp_dequeue_style( 'wc-block-style' ); // Remove WooCommerce block CSS
+		wp_dequeue_style( 'wc-blocks-style' ); // Remove WooCommerce block CSS
 
+		wp_deregister_script( 'jquery-migrate' );
 	}
 }
 add_action( 'wp_enqueue_scripts', 'amp_load_styles_and_scripts' );
